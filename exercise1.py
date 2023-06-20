33610467fedc14d145ecc70d6767c728934bbecc
ChhitizV <chhitizvaidya10@gmail.com>
2023-06-20 20:52:12 +0545

    1

---
+++ exercise1.py
@@ -0,0 +1,8 @@
+# Program to add two numbers
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+sum = num1 + num2
+
+print(f"The sum of {num1} + {num2} is {sum}")
