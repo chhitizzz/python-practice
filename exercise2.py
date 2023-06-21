76e28b1574ca166bc0a418f980b96edfcce97920
ChhitizV <chhitizvaidya10@gmail.com>
2023-06-21 22:36:58 +0545

    2

--- exercise2.py
+++ exercise2.py
@@ -6,3 +6,13 @@ num1 = int(input("Enter a number: "))
 sqr = num1 ** (1/2) 
 print(f"The square root of {num1} is {sqr}")
 
+print("")
+
+#! Solution 2: Solving using Math Module 
+import math
+
+num2 = int(input("Enter a number: "))
+
+sr = math.sqrt(num2)
+
+print(f"The square root of {num2} is {sr}")
