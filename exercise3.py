788c9d8ee0a4426d57e8bf9400a435d4abfe7d00
ChhitizV <chhitizvaidya10@gmail.com>
2023-06-22 20:33:54 +0545

    3

--- exercise3.py
+++ exercise3.py
@@ -1,2 +1,10 @@
 # Program to calculate area of triangle
 
+a = float(input("Enter first side: "))
+b = float(input("Enter second side: "))
+c = float(input("Enter third side: "))
+
+# Semi Perimeter 
+s = (a + b + c) / 2 
+
+# Area of triangle
