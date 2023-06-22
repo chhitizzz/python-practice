9d24d8328b21f1b59cffec3d4e033ade9a6f793c
ChhitizV <chhitizvaidya10@gmail.com>
2023-06-22 20:37:14 +0545

    3

--- exercise3.py
+++ exercise3.py
@@ -4,7 +4,10 @@ a = float(input("Enter first side: "))
 b = float(input("Enter second side: "))
 c = float(input("Enter third side: "))
 
-# Semi Perimeter 
+# Semi-Perimeter 
 s = (a + b + c) / 2 
 
 # Area of triangle
+area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+
+print(f"The area of the triangle is {area}")
