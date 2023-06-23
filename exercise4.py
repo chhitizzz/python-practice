f92aaa01cd5077140c1ddadadbdc20db551ff0ba
ChhitizV <chhitizvaidya10@gmail.com>
2023-06-23 22:31:19 +0545

    4

--- exercise4.py
+++ exercise4.py
@@ -1,12 +1,25 @@
 # Program to swap two variables 
 
 #! Solution 1: Using third variable
+
 x = int(input("Enter first number: "))
 y = int(input("Enter second number: "))
 
 temp = x 
 x = y
 y = temp
+
+print(f"The value of x is {x}")
+print(f"The value of y is {y}")
+
+print("")
+
+#! Solution 2: Without using third variable 
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+a, b = b, a
 
 print(f"The value of x is {x}")
 print(f"The value of y is {y}")
