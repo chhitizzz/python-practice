5792e57def8b030ec3e8eeaccf0b799ac33f997b
ChhitizV <chhitizvaidya10@gmail.com>
2023-06-25 21:39:03 +0545

    6

--- exercise6.py
+++ exercise6.py
@@ -2,3 +2,11 @@
 
 num = int(input("Enter a number: "))
 
+if num > 0:
+    print(f"{num} is a positive number")
+
+elif num == 0:
+    print("Zero")
+
+else:
+    print(f"{num} is a negative number")
