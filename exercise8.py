33568d28769e8815086fa997db47258b9684362d
ChhitizV <chhitizvaidya10@gmail.com>
2023-06-27 22:10:59 +0545

    8

--- exercise8.py
+++ exercise8.py
@@ -1,2 +1,14 @@
 # Program to find largest number among three numbers
 
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+
+if (num1 > num2) and (num1 > num3):
+    print(f"{num1} is the largest number")
+
+elif (num2 > num1) and (num2 > num3):
+    print(f"{num2} is the largest number")
+
+else:
+    print(f"{num3} is the largest number")
