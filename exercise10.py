8f5fe1bedd4d5b9f19b0c92fa3564674dd851a73
ChhitizV <chhitizvaidya10@gmail.com>
2023-06-29 19:58:33 +0545

    10

--- exercise10.py
+++ exercise10.py
@@ -1,2 +1,11 @@
 # Program to check if a character is a vowel or consonant
 
+character = input("Enter a letter: ")
+
+vowel = ['a', 'e', 'i', 'o', 'u']
+
+if character == vowel:
+    print(f"{character} is a vowel")
+
+else:
+    print(f"{character} is a consonant")
