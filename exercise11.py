f12500b821f4f32e117c51e0131107e38db4a01d
ChhitizV <chhitizvaidya10@gmail.com>
2023-06-30 12:47:21 +0545

    11

--- exercise11.py
+++ exercise11.py
@@ -6,6 +6,10 @@ if number > 1:
     for i in range (2, number):
         if number % i == 0:
             print(f"{number} is not a prime number")
+            break
 
     else:
         print(f"{number} is a prime number")
+
+else:
+    print(f"{number} is not a prime number")
