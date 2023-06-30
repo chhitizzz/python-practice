6c981f34d633933133360dc72d7ca4f69269c1b2
ChhitizV <chhitizvaidya10@gmail.com>
2023-06-30 12:45:12 +0545

    11

--- exercise11.py
+++ exercise11.py
@@ -1,1 +1,8 @@
 # Program to find if a given number is a prime number or not 
+
+number = int(input("Enter a number: "))
+
+if number > 1:
+    for i in range (2, number):
+        if number % i == 0:
+            print(f"{number} is not a prime number")
