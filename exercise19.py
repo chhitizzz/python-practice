2bb0e1dbc1998fb60137e881d030db6d21aadda1
ChhitizV <chhitizvaidya10@gmail.com>
2023-07-08 17:02:35 +0545

    19

--- exercise19.py
+++ exercise19.py
@@ -1,4 +1,8 @@
 # Program that generates a random number between 1 and 10 and asks the user to guess the number. The program should continue asking for guesses until the user guesses correctly.
 
+import random
+
+secret_number = random.randint(1, 10)
+
 num = int(input("Guess a number between 1 to 10: "))
 
