4628898643e12f0f3626cd24554bab0e4e5bfee0
ChhitizV <chhitizvaidya10@gmail.com>
2023-07-08 17:07:43 +0545

    19

--- exercise19.py
+++ exercise19.py
@@ -1,13 +1,13 @@
-# Program that generates a random number between 1 and 20 and asks the user to guess the number. The program should continue asking for guesses until the user guesses correctly.
+# Program that generates a random number between 1 and 10 and asks the user to guess the number. The program should continue asking for guesses until the user guesses correctly.
 
 import random
 
-secret_number = random.randint(1, 20)
+secret_number = random.randint(1, 10)
 
 guess_number = 0
 
 while guess_number != secret_number:
-    guess_number = int(input("Guess the number (between 1 and 20): "))
+    guess_number = int(input("Guess the number (between 1 and 10): "))
 
     if guess_number == secret_number:
         print("Congratulations! You have guessed the correct number.")
@@ -15,4 +15,4 @@ while guess_number != secret_number:
     else:
         print("Wrong guess. Try again.")
 
-
+print("Game over.")
