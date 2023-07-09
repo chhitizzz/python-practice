4a3c77396a35dc0eae9ce9d5c564661e2fb2fb65
ChhitizV <chhitizvaidya10@gmail.com>
2023-07-09 18:14:09 +0545

    20

--- exercise20.py
+++ exercise20.py
@@ -1,1 +1,3 @@
 # Program that asks the user for a number and uses a loop to print a triangle pattern of asterisks. The number of rows in the triangle should be equal to the entered number.
+
+num = int(input("Enter a number: "))
