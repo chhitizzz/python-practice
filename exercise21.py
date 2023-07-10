7db98415611e84e14111b8bf1af4ceb1e2c0a602
ChhitizV <chhitizvaidya10@gmail.com>
2023-07-10 21:29:57 +0545

    21

--- exercise21.py
+++ exercise21.py
@@ -3,3 +3,8 @@
 string = input("Enter a string: ")
 no_of_vowels = 0
 
+for char in string:
+    if char.lower() in 'aeiou':
+        no_of_vowels += 1
+
+print(f"The number of vowels in {string} is {no_of_vowels}.")
