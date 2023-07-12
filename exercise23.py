d76b80f23a4a1ef71d8e57bdec34de88496e503e
ChhitizV <chhitizvaidya10@gmail.com>
2023-07-12 18:49:46 +0545

    23

--- exercise23.py
+++ exercise23.py
@@ -1,3 +1,12 @@
 # Program to display the multiplication table 
 
 #! Method 1: Using for loop 
+
+num = int(input("Enter a number: "))
+
+for i in range (1, 11):
+    print(f"{num} x {i} = {num * i}")
+
+print("")
+
+#! Method 2: Using while loop 
