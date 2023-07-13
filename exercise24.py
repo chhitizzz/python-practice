d5e79f9edf255cd5c922f657013156538ed8584c
ChhitizV <chhitizvaidya10@gmail.com>
2023-07-13 11:43:08 +0545

    24

--- exercise24.py
+++ exercise24.py
@@ -10,3 +10,12 @@ num = int(input("Enter a number to obtain the Fibonacci Sequence: "))
 
 if num == 1:
     print(a)
+
+else: 
+    print(a)
+    print(b)
+    for i in range(1, num + 1):
+        c = a + b
+        a = b
+        b = c
+        print(c)
