c928a30d9174d22371222a39534fa52b93213172
ChhitizV <chhitizvaidya10@gmail.com>
2023-07-14 17:30:51 +0545

    25

--- exercise25.py
+++ exercise25.py
@@ -10,13 +10,14 @@ An example of an Armstrong number is:
 '''
 
 num = int(input("Enter a number: "))
+order = len(str(num))
 
 sum = 0
 temp = num 
 
 while temp > 0:
     digit = temp % 10
-    cube = digit ** 3
+    cube = digit ** order
     sum = sum + cube
     temp //= 10
 
