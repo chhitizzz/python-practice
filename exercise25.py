f0edc6e93fc94860b8971276ac2278aeaa454dfa
ChhitizV <chhitizvaidya10@gmail.com>
2023-07-14 17:21:46 +0545

    25

--- exercise25.py
+++ exercise25.py
@@ -1,1 +1,10 @@
 # Program to check Armstrong number
+
+'''
+Armstrong number: is a number that is the sum of its own digits each raised to the power of the number of digits. 
+
+An example of an Armstrong number is: 
+153 = (1 x 1 x 1) + (5 x 5 x 5) + (3 x 3 x 3) 
+153 = 1 + 125 + 27
+153 = 153
+'''
