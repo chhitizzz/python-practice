bc983a4f829efa7635bb4f630794393b1a2a0c51
ChhitizV <chhitizvaidya10@gmail.com>
2023-07-15 22:05:02 +0545

    26

--- exercise26.py
+++ exercise26.py
@@ -3,5 +3,9 @@
 lower = int(input("Enter the lower limit: "))
 upper = int(input("Enter the upper limit: "))
 
-sum = 0
 
+for num in range(lower, upper + 1):
+    sum = 0
+    temp = num 
+    while temp > 0: 
+        digit = temp % 10
