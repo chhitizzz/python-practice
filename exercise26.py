00dc49c4ff5949e35e1dd0ed3771cd548f4c9433
ChhitizV <chhitizvaidya10@gmail.com>
2023-07-15 22:08:54 +0545

    26

--- exercise26.py
+++ exercise26.py
@@ -5,7 +5,14 @@ upper = int(input("Enter the upper limit: "))
 
 
 for num in range(lower, upper + 1):
+    order = len(str(num))
     sum = 0
     temp = num 
+
     while temp > 0: 
         digit = temp % 10
+        sum += digit ** order 
+        temp //= 10
+        
+    if num == sum:
+        print(f"The Armstrong number ranging from {lower} to {upper} is: {num}")
