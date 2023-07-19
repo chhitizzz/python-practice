442b004dd92e290b71d77b0b6cd7e6ae249f0f5a
ChhitizV <chhitizvaidya10@gmail.com>
2023-07-19 21:38:56 +0545

    30

--- exercise30.py
+++ exercise30.py
@@ -1,1 +1,8 @@
 # Program to concatenate two strings
+
+string1 = input("Enter first string: ")
+string2 = input("Enter second string: ")
+
+concatenated_string = string1 + string2
+
+print("The concatenated string is ", concatenated_string)
