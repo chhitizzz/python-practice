5a782220228841b388d159a090c238dc54b51569
ChhitizV <chhitizvaidya10@gmail.com>
2023-07-20 12:07:33 +0545

    31

--- exercise31.py
+++ exercise31.py
@@ -1,6 +1,9 @@
 # Program to reverse a string
 
 def reverse_string(input_string):
-    return input[::-1]
+    return input_string[::-1]
 
 string = input("Enter a string: ")
+reverse_string = reverse_string(string)
+
+print(f"Reversed string: {reverse_string}")
