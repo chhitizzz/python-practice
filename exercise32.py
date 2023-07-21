bb1760efb4c99230f8a8f83407f159b2bd3b94ae
ChhitizV <chhitizvaidya10@gmail.com>
2023-07-21 16:25:33 +0545

    32

--- exercise32.py
+++ exercise32.py
@@ -3,3 +3,9 @@
 string = input("Enter a string: ")
 vowels = "aeiouAEIOU"
 result = ""
+
+for char in string:
+    if char not in vowels: 
+        result += char
+
+print("String without vowels:", result)
