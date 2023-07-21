436a2249c6d130d317e7ef72087add02a67d8c8e
ChhitizV <chhitizvaidya10@gmail.com>
2023-07-21 16:10:30 +0545

    32

--- exercise32.py
+++ exercise32.py
@@ -1,1 +1,8 @@
 # Program to remove the vowels from a string
+
+string = input("Enter a string: ")
+
+replacements = [('a', ""), ("e", ""), ("i", ""), ("o", ""), ("u", "")]
+
+for char, replacement in replacements:
+    pass
