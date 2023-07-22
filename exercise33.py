02febe78ba435b648d6b38dfa92c29d16fee48b9
ChhitizV <chhitizvaidya10@gmail.com>
2023-07-22 18:01:47 +0545

    33

--- exercise33.py
+++ exercise33.py
@@ -1,10 +1,12 @@
 # Program to count the occurrences of a specific character in a string
 
 user_input = input("Enter a string: ")
-characters_to_count = input("Enter the number of characters to count: ")
+characters_to_count = input("Enter the character to count: ")
 
 count = 0
 
 for char in user_input:
     if user_input == characters_to_count:
         count += 1
+
+print(f"The character '{characters_to_count}' appears {count} times in {user_input}.")
