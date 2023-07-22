c70a45da4ceb12c620a8512cad76bdc505b12f16
ChhitizV <chhitizvaidya10@gmail.com>
2023-07-22 18:02:19 +0545

    33

--- exercise33.py
+++ exercise33.py
@@ -6,7 +6,7 @@ characters_to_count = input("Enter the character to count: ")
 count = 0
 
 for char in user_input:
-    if user_input == characters_to_count:
+    if char == characters_to_count:
         count += 1
 
 print(f"The character '{characters_to_count}' appears {count} times in {user_input}.")
