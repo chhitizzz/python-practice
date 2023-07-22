60b359b012418e08c48775b65c78b63f15ebf8b2
ChhitizV <chhitizvaidya10@gmail.com>
2023-07-22 17:59:00 +0545

    33

--- exercise33.py
+++ exercise33.py
@@ -3,3 +3,8 @@
 user_input = input("Enter a string: ")
 characters_to_count = input("Enter the number of characters to count: ")
 
+count = 0
+
+for char in user_input:
+    if user_input == characters_to_count:
+        count += 1
