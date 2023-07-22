45b391dbe0957b2e98c1cbc16f7300b50a614b3d
ChhitizV <chhitizvaidya10@gmail.com>
2023-07-22 17:58:06 +0545

    33

--- exercise33.py
+++ exercise33.py
@@ -1,1 +1,5 @@
 # Program to count the occurrences of a specific character in a string
+
+user_input = input("Enter a string: ")
+characters_to_count = input("Enter the number of characters to count: ")
+
