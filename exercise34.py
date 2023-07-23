e569805d23ef7851a4a3eef62ed038793c9f006a
ChhitizV <chhitizvaidya10@gmail.com>
2023-07-23 14:32:46 +0545

    34

--- exercise34.py
+++ exercise34.py
@@ -1,1 +1,7 @@
 # Program to check if a given string is an anagram of another string
+
+# Anagrams are formed by rearranging the letters of a word to create a new word, using all the original letters exactly once.
+
+string1 = input("Enter first string: ")
+string2 = input("Enter second string: ")
+
