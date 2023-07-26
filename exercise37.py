071a3bbc67d781adcb227542e2b8c26c1e58075a
ChhitizV <chhitizvaidya10@gmail.com>
2023-07-26 21:06:20 +0545

    37

--- exercise37.py
+++ exercise37.py
@@ -5,3 +5,6 @@ prefix_to_check = input("Enter a prefix to check: ")
 
 if user_input.startswith(prefix_to_check):
     print(f"The string '{user_input}' starts with the prefix '{prefix_to_check}'.")
+
+else: 
+    print(f"The string '{user_input}' doesn't starts with the prefix '{prefix_to_check}'.")
