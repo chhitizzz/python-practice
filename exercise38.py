57565aa9562114fd0817362bb4d84cd6af77e042
ChhitizV <chhitizvaidya10@gmail.com>
2023-07-27 21:42:25 +0545

    38

--- exercise38.py
+++ exercise38.py
@@ -4,7 +4,7 @@ user_input = input("Enter a string: ")
 suffix_to_check = input("Enter a suffix to check: ")
 
 if user_input.endswith(suffix_to_check):
-    print(f"The string {user_input} ends with the suffix '{suffix_to_check}.'")
+    print(f"The string {user_input} ends with the suffix '{suffix_to_check}'.")
 
 else: 
-    print(f"The string {user_input} doesn't end with the suffix '{suffix_to_check}.'")
+    print(f"The string {user_input} doesn't end with the suffix '{suffix_to_check}'.")
