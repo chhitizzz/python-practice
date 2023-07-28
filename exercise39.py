ae531aa28b51375834db89e51a06f63349885e56
ChhitizV <chhitizvaidya10@gmail.com>
2023-07-28 20:56:37 +0545

    39

--- exercise39.py
+++ exercise39.py
@@ -3,3 +3,7 @@
 file_name = input("Enter a file name: ")
 
 dot_index = file_name.rfind(".")
+
+if dot_index != -1:
+    file_extension = file_name[dot_index + 1:]
+    print(f"File extension: {file_extension}")
