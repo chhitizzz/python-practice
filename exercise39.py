cca1ba50b12f7186df0caf6ec0409ad32e03cf11
ChhitizV <chhitizvaidya10@gmail.com>
2023-07-28 20:57:42 +0545

    39

--- exercise39.py
+++ exercise39.py
@@ -7,3 +7,6 @@ dot_index = file_name.rfind(".")
 if dot_index != -1:
     file_extension = file_name[dot_index + 1:]
     print(f"File extension: {file_extension}")
+
+else:
+    print("Invalid file name. No extension found.")
