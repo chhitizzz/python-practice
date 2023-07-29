cecf1c2d718f6138963b8ae4872f8ca27669eb0b
ChhitizV <chhitizvaidya10@gmail.com>
2023-07-29 21:03:48 +0545

    40

--- exercise40.py
+++ exercise40.py
@@ -1,2 +1,9 @@
 # Program to replace all occurrences of a specific word in a string with another word
 
+user_input = input("Enter a string: ")
+word_to_replace = input("Enter the word to replace: ")
+replacement_word = input("Enter the replacement word: ")
+
+words = user_input.split()
+
+
