87650395e34cbc7eccb462ad83d7ec353872c05a
ChhitizV <chhitizvaidya10@gmail.com>
2023-07-29 21:08:42 +0545

    40

--- exercise40.py
+++ exercise40.py
@@ -6,4 +6,10 @@ replacement_word = input("Enter the replacement word: ")
 
 words = user_input.split()
 
+for i in range(len(words)):
+    if words[i] == word_to_replace:
+        words[i] = replacement_word
 
+result = ' '.join(words)
+
+print("Result:", result)
