0a6ab7be1620feeeee4e77e2b05294b10933b0a0
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-01 12:42:02 +0545

    43

--- exercise43.py
+++ exercise43.py
@@ -1,1 +1,7 @@
 # Program to find the length of a list
+
+courses = ['Maths', 'Physics', 'Chemistry', 'Biology']
+
+length_of_the_list = len(courses)
+
+print("The length of the list is", length_of_the_list)
