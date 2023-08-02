f10baf170ccd044c03273b75a5432c5f3a029446
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-02 12:51:21 +0545

    44

--- exercise44.py
+++ exercise44.py
@@ -7,3 +7,6 @@ n = int(input("Enter the index of the element to access starting from 0: "))
 if 0 <= n < len(randomList):
     element = randomList[n]
     print(f"The element at index {n} is: {element}")
+
+else: 
+    print("Invalid index. The list doesn't have an element at that index.")
