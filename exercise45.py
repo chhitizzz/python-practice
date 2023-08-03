293661b02e0da0cd12fb134eb411b98adb55dd5a
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-03 21:10:55 +0545

    45

--- exercise45.py
+++ exercise45.py
@@ -3,3 +3,9 @@
 randomList = [10, 20, 30, 40, 50]
 
 element_to_append = int(input("Enter an element to append to the list: "))
+
+print(f"List before append: {randomList}")
+
+randomList.append(element_to_append)
+
+print(f"Updated list: {randomList}")
