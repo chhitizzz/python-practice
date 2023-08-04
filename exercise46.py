cf33f2a5fed13832abd760d1cdde6f7a125a94c7
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-04 09:43:51 +0545

    46

--- exercise46.py
+++ exercise46.py
@@ -1,1 +1,7 @@
 # Program to remove an element from a list
+
+randomList = [10, 20, 30, 40]
+
+removeElement = int(input("Enter the element to remove from the list: "))
+
+print(f"List before removing: {randomList}")
