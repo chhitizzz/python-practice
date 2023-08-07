83aab57daa9cb712cbb5078302215c02c09450af
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-07 09:00:14 +0545

    49

--- exercise49.py
+++ exercise49.py
@@ -4,4 +4,6 @@ myList = [20, 10, 40, 50, 30, 70, 80, 60]
 
 print("List: ", myList)
 
-print(max(myList))
+print("The maximum element in the list is", max(myList)) # 80
+
+print("The minimum element in the list is", min(myList)) # 10
