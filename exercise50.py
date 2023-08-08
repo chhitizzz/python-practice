f1cae79db63e0cdfd90ce3647fb9f8ec38b3f182
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-08 10:19:55 +0545

    50

--- exercise50.py
+++ exercise50.py
@@ -3,3 +3,11 @@
 myList = [10, 20, 30, 40, 50, 60]
 
 print("List: ", myList)
+
+elementExists = int(input("Enter the element to check in the list: "))
+
+if elementExists in myList:
+    print("The element exists in the list.")
+
+else: 
+    print("The element doesn't exist in the list.")
