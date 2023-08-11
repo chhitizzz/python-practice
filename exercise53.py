40eb83c6c683e9fc38c921b2e4cf0543f9e38caa
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-11 10:26:25 +0545

    53

--- exercise53.py
+++ exercise53.py
@@ -1,1 +1,7 @@
 # Program to find the length of a tuple
+
+subjects = ('History', 'Maths', 'Biology', 'Computer Science', 'Physics')
+
+print("The tuple is:", subjects)
+
+print("The length of the tuple is:", len(subjects))
