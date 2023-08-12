a549ae475f1af5de1742e87edec082f80e128efe
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-12 21:14:04 +0545

    54

--- exercise54.py
+++ exercise54.py
@@ -1,1 +1,5 @@
-# Program to access the nth element of a tuple
+# Program to access the nth element of a tuple\
+
+randomTuple = ('France', 10, 'Mbappe', 19, 'Lucario')
+
+print("The tuple is", randomTuple)
