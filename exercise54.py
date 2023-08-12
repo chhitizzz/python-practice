cf2505234deae7212df8fb776a2ee37e2e433791
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-12 21:16:45 +0545

    54

--- exercise54.py
+++ exercise54.py
@@ -1,4 +1,4 @@
-# Program to access the nth element of a tuple\
+# Program to access the nth element of a tuple
 
 randomTuple = ('France', 10, 'Mbappe', 19, 'Lucario')
 
@@ -9,3 +9,6 @@ n = int(input("Enter the index of the element to access starting from 0: "))
 if 0 <= n < len(randomTuple):
     element = randomTuple[n]
     print(f"The element at index {n} is: {element}")
+
+else: 
+    print("Invalid index. The tuple doesn't have an element at that index.")
