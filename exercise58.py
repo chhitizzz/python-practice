e63c749b0588a4fc57d2d6e46167c867c5df4393
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-16 09:11:43 +0545

    58

--- exercise58.py
+++ exercise58.py
@@ -1,1 +1,7 @@
 # Program to count the occurrences of an element in a tuple
+
+myTuple = (10, 20, 30, 20, 30, 10)
+
+print("The tuple is:", myTuple)
+
+elementToCount = int(input("Enter the element to count: "))
