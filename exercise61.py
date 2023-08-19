435420dc9867bcc9d682f3f2b7d870367bb4ede8
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-19 16:26:16 +0545

    61

--- exercise61.py
+++ exercise61.py
@@ -1,1 +1,9 @@
 # Program to slice a tuple.
+
+myTuple = (10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
+
+print("The tuple is:", myTuple)
+
+sliced_tuple = myTuple[3: 9]
+
+print("Sliced tuple:", sliced_tuple)
