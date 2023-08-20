7b533e72e25f956bc2ed883b62d777c8308f4481
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-20 21:41:06 +0545

    62

--- exercise62.py
+++ exercise62.py
@@ -1,1 +1,8 @@
 # Function that takes two numbers as arguments and returns their sum
+
+def calculateSum(x, y):
+    sum = x + y
+    return sum
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
