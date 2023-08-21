7a4a7f0751ab1821eaf0d890a4687c94e048011b
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-21 21:33:05 +0545

    63

--- exercise63.py
+++ exercise63.py
@@ -8,9 +8,17 @@ def calculate_average(numbers):
     average = total / len(numbers)
     return average
 
-num_count = int(input("Enter the number of elements:"))
+num_count = int(input("Enter the number of elements: "))
 num_list = []
 
 for i in range(num_count):
     num = float(input(f"Enter number {i + 1}: "))
     num_list.append(num)
+
+average_result = calculate_average(num_list)
+
+if average_result is not None: 
+    print(f"The average of the numbers is {average_result:.2f}")
+
+else:
+    print("The list is empty.")
