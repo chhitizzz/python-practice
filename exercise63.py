024a78467261c162ab0e536f7cfb6255ec1add23
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-21 21:28:36 +0545

    63

--- exercise63.py
+++ exercise63.py
@@ -1,1 +1,9 @@
 # Function called `calculate_average` that takes a list of numbers as an argument and returns the average (mean) of the numbers
+
+def calculate_average(numbers):
+    if not numbers:
+        return None
+    
+    total = sum(numbers)
+    average = total / len(numbers)
+    return average
