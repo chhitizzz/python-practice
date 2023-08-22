f0510d1bd1e52433eb6473bbb33d9dfae4f9b37c
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-22 21:34:45 +0545

    64

--- exercise64.py
+++ exercise64.py
@@ -3,3 +3,8 @@
 def stringLength(str):
     length_of_string = len(str) 
     return length_of_string
+
+input_string = input("Enter a string: ")
+
+result = stringLength(input_string)
+print(f"The length of the string is {result}.")
