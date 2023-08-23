66e516e41956205d07122d5b49032e57ef0d831a
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-23 21:40:17 +0545

    65

--- exercise65.py
+++ exercise65.py
@@ -16,5 +16,7 @@ for i in range(numCount):
     num = int(input(f"Enter number {i + 1}: "))
     numList.append(num)
 
+print(f"The list is: {numList}")
+
 result = even_numbers_sum(numList)
-print(f"The sum of even numbers is {result}")
+print(f"The sum of even numbers is {result}.")
