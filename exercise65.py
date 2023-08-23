fbc2e3ba0fe5ca899d33209a10d5ac6f8ed650be
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-23 21:35:15 +0545

    65

--- exercise65.py
+++ exercise65.py
@@ -1,1 +1,8 @@
 # Function that takes a list of numbers as an argument and returns the sum of all even numbers in the list
+
+def even_numbers_sum (numbers):
+    evenSum = 0
+
+    for num in numbers:
+        if num % 2 == 0:
+            evenSum += num
