5b4eabadcc31927120d415eef2820d7ad4c48ebd
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-24 21:19:41 +0545

    66

--- exercise66.py
+++ exercise66.py
@@ -8,3 +8,12 @@ def find_string_with_substring(stringList, searchString):
             result_list.append(string)
 
     return result_list
+
+string_count = int(input("Enter the number of strings: "))
+string_list = []
+
+for i in range(string_count):
+    string = input(f"Enter string {i + 1}: ")
+    string_list.append(string)
+
+search_string = input("Enter the search string: ")
