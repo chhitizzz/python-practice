390b9e0a713a5e303abf06cbca3b72f81908eca2
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-24 21:17:55 +0545

    66

--- exercise66.py
+++ exercise66.py
@@ -1,1 +1,10 @@
 # Function that takes a list of strings and a search string as arguments and returns a list of strings containing the search string
+
+def find_string_with_substring(stringList, searchString):
+    result_list = []
+
+    for string in stringList:
+        if searchString in string: 
+            result_list.append(string)
+
+    return result_list
