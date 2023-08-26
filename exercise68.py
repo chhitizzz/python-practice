b5edbcceea25e81b7914a94f798073c88823b01c
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-26 21:38:01 +0545

    68

--- exercise68.py
+++ exercise68.py
@@ -1,1 +1,10 @@
 # Function that takes a list of numbers as an argument and returns a new list containing only the positive numbers
+
+def positiveNumbers(numbers):
+    positive_numbers = []
+
+    for num in numbers: 
+        if num > 0:
+            positive_numbers.append(num)
+        
+    return positive_numbers
