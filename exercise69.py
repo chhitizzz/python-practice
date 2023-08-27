c5e143b9fbd9e6b67c3507800ec81c9ac204b658
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-27 21:24:23 +0545

    69

--- exercise69.py
+++ exercise69.py
@@ -1,1 +1,8 @@
 # Function that calculates the factorial of a given number using recursion
+
+def calculateFactorial(number):
+    if number == 0:
+        return 1 
+    
+    else: 
+        return number * calculateFactorial(number - 1)
