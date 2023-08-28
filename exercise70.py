cde6cd08cc832e2e99b0aa8e62cd11b546fca707
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-28 23:11:38 +0545

    70

--- exercise70.py
+++ exercise70.py
@@ -1,1 +1,13 @@
 # Function that takes a string as an argument and returns a dictionary with the count of each character in the string
+
+def count_characters(string):
+    char_count = {}
+
+    for char in string: 
+        if char in char_count:
+            char_count[char] += 1
+        
+        else: 
+            char_count[char] = 1
+
+    return char_count
