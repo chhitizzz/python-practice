081a1b597babda2ee5e602f79b88fda63e2768d8
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-29 21:48:18 +0545

    71

--- exercise71.py
+++ exercise71.py
@@ -3,3 +3,9 @@
 def calculate_power(base, exponent=2):
     result = base ** exponent
     return result 
+
+base_number = int(input("Enter the base number: "))
+exponent_number = int(input("Enter the exponent number: "))
+
+result = calculate_power(base_number, exponent_number)
+print(f"{base_number} raised to the power of {exponent_number} is {result}")
