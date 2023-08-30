6a75c9ffb09a25352e66e2237f861641964b87fd
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-30 14:22:48 +0545

    72

--- exercise72.py
+++ exercise72.py
@@ -4,3 +4,8 @@ def uniqueWords(sentence):
     words = sentence.lower().split()
     unique_words = set(words)
     return list(unique_words)
+
+input_sentence = input("Enter a sentence: ")
+
+result = uniqueWords(input_sentence)
+print("Unique words:", result)
