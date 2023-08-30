290704c4881c5edc82747845bf0e456f4fbddcc8
ChhitizV <chhitizvaidya10@gmail.com>
2023-08-30 14:21:17 +0545

    72

--- exercise72.py
+++ exercise72.py
@@ -1,1 +1,6 @@
 # Function that takes a sentence as an argument and returns a list of unique words. Allow the function to ignore case
+
+def uniqueWords(sentence):
+    words = sentence.lower().split()
+    unique_words = set(words)
+    return list(unique_words)
