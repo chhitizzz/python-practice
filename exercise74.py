5d988a32004008d4418b0f25878640d29b70c713
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-01 21:29:19 +0545

    74

--- exercise74.py
+++ exercise74.py
@@ -1,1 +1,7 @@
 # Function that takes a list of numbers as an argument and returns the minimum and maximum values as a tuple
+
+def findMinMax(numbers):
+    if not numbers: 
+        return None
+
+        
