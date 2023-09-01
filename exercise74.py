38b13db044e24b4198bde61faee717059916a3bf
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-01 21:32:23 +0545

    74

--- exercise74.py
+++ exercise74.py
@@ -8,3 +8,10 @@ def findMinMax(numbers):
     max_value = max(numbers)
 
     return(min_value, max_value)
+
+num_count = int(input("Enter the number of elements: "))
+num_list = []
+
+for i in range(num_count):
+    num = float(input(f"Enter number {i + 1}: "))
+    num_list.append(num)
