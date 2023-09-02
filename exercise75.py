158b6563053831b8f579ead01bfb1a6c9794b4a4
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-02 20:50:14 +0545

    75

--- exercise75.py
+++ exercise75.py
@@ -1,1 +1,11 @@
 # Function that takes a string as an argument and returns the first and last characters as a tuple
+
+def firstLastChar(string):
+    if not string:
+        return None
+    
+    firstChar = string[0]
+    lastChar = string[-1]
+
+    return(firstChar, lastChar)
+
