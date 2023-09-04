d8a4d50b46b8d05a56cae197800e5e5a341f3e16
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-04 20:34:23 +0545

    77

--- exercise77.py
+++ exercise77.py
@@ -1,1 +1,15 @@
-# Function that generates a random password of a given length. Allow the function to include letters, digits, and special characters.
+# Function that generates a random password of a given length. Allow the function to include letters, digits, and special characters
+
+import random 
+import string 
+
+def generatePassword(length):
+    letters = string.ascii_letters
+    digits = string.digits
+    special_characters = string.punctuation
+
+    character = letters + digits + special_characters
+
+    password = ''.join(random.choice(character) for _ in range(length))
+
+    return password
