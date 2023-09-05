326a100d6f6473e45b0481b7733c3a9bec0b94b9
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-05 22:26:39 +0545

    78

--- exercise78.py
+++ exercise78.py
@@ -1,1 +1,11 @@
 # Recursive function to calculate the nth Fibonacci number.
+
+def fibonacci_sequence(n):
+    if n <= 0:
+        return 0
+    
+    elif n == 1:
+        return 1
+
+    else:
+        return fibonacci_sequence(n - 1) + fibonacci_sequence(n - 2)
