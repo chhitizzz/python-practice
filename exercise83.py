2ae2edd54c7106655ea295f8b68110022ec9757b
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-10 11:47:20 +0545

    83

--- exercise83.py
+++ exercise83.py
@@ -1,1 +1,7 @@
 # Program to find the length of a set
+
+vegetables = {'tomato', 'potato', 'cauliflower', 'brinjal'}
+
+length_of_set = len(vegetables)
+
+print(f"The length of the set is {length_of_set}")
