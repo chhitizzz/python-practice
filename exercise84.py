693c19e5f04d0dbaf909f76d3b75d16c75e02741
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-11 12:11:15 +0545

    84

--- exercise84.py
+++ exercise84.py
@@ -1,1 +1,9 @@
 # Program to add an element to a set
+
+asia_cup_team = {'Nepal', 'India', 'Pakistan', 'Sri Lanka'}
+
+print("The set before adding an element: ", asia_cup_team)
+
+asia_cup_team.add('Bangladesh')
+
+print("The set after adding an element: ", asia_cup_team)
