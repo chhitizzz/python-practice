f0ebb8760c5c9df22b856c9e46a5ac49c8df4bfc
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-12 22:11:03 +0545

    85

--- exercise85.py
+++ exercise85.py
@@ -1,1 +1,9 @@
 # Program to remove an element from a set
+
+players = {"Ronaldo", "Messi", "Mbappe", "Rashford"}
+
+print(f"Original set: {players}")
+
+players.remove("Rashford")
+
+print(f"Set after removal of element: {players}")
