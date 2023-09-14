1497d1756d1dc105cbef7e24f5e387679c8bd4f4
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-14 10:36:33 +0545

    87

--- exercise87.py
+++ exercise87.py
@@ -1,1 +1,11 @@
 # Program to check if an element exists in a set
+
+numbers = {10, 20, 30, 40, 50}
+
+element_to_check = int(input("Enter the element to check in the set: "))
+
+if element_to_check in numbers:
+    print(f"{element_to_check} exists in the set.")
+
+else:
+    print(f"{element_to_check} doesn't exist in the set.")
