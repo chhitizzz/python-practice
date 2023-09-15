4d9861d62ac887bfdbd6a213866a673d334cb8f8
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-15 13:13:01 +0545

    88

--- exercise88.py
+++ exercise88.py
@@ -1,1 +1,8 @@
 # Program to find the difference between two sets
+
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+
+result = set1 - set2
+
+print("The difference between two sets is", result)
