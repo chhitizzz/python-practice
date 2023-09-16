dcd23a45189339c305830f8d185e4a367458c46a
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-16 17:22:31 +0545

    89

--- exercise89.py
+++ exercise89.py
@@ -1,1 +1,10 @@
 # Program to check if two sets are equal or not
+
+set1 = {10, 20, 30, 40, 50}
+set2 = {20, 30, 40, 50, 60}
+
+if (set1 == set2):
+    print("The sets are equal.")
+
+else:
+    print("The sets are not equal.")
