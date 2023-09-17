c23f637969236e1a1fd63e01b41403128755a585
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-17 20:13:38 +0545

    90

--- exercise90.py
+++ exercise90.py
@@ -6,3 +6,10 @@ def remove_duplicate(input_list):
     unique_list = list(unique_set)
 
     return unique_list
+
+input_list = [10, 20, 30, 10, 40, 50, 20]
+
+result = remove_duplicate(input_list)
+
+print("Original list: ", input_list)
+print("List without duplicates: ", result)
