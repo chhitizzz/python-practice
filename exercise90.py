077b3afe2efbf153b009bbf2a7a895f5255b90c6
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-17 20:11:43 +0545

    90

--- exercise90.py
+++ exercise90.py
@@ -1,1 +1,8 @@
 # Program to remove duplicates from a list using a set
+
+def remove_duplicate(input_list):
+    unique_set = set(input_list)
+
+    unique_list = list(unique_set)
+
+    return unique_list
