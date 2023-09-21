bd8dd82afcf34edc13142b983f6c8d3c603fda84
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-21 07:13:26 +0545

    94

--- exercise94.py
+++ exercise94.py
@@ -1,7 +1,15 @@
 # Program to access the value associated with a specific key in a dictionary
 
+def accessDictionary(dictionary, key):
+    if key in dictionary:
+        return dictionary[key]
+    
+    else:
+        return None
+
 myDict = {
     'name': 'Subhu',
     'age': 22,
     'country': 'Nepal'
 }
+
