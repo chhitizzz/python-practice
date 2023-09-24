92a84d7808f855c6e0bae077d39d7485bbbe7efc
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-24 22:13:11 +0545

    97

--- exercise97.py
+++ exercise97.py
@@ -1,7 +1,12 @@
 # Program to check if a given key exists in a dictionary
 
+def check_key_existence(dictionary, key):
+    return key in dictionary    
+
 myDict = {
     'firstName': 'Joe',
     'lastName': 'Mama',
     'age': 27
 }
+
+key_to_check = 'firstName'
