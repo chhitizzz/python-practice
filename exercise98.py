a4db148e75ad1eacbee40e0c4be23de1c3feb23b
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-25 21:52:03 +0545

    98

--- exercise98.py
+++ exercise98.py
@@ -1,1 +1,14 @@
 # Program to get a list of all keys in a dictionary
+
+def get_all_keys(dictionary):
+    return list(dictionary.keys())
+
+myDict = {
+    'name': 'Rodgers',
+    'state': 'Wisconsin',
+    'team': 'Packers'
+}
+
+keys_list = get_all_keys(myDict)
+
+print("List of keys:", keys_list)
