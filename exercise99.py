91dc9e99de85bd13f378c71bbb8eb13b576c8ae2
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-26 21:25:13 +0545

    99

--- exercise99.py
+++ exercise99.py
@@ -1,1 +1,10 @@
 # Program to get a list of all values in a dictionary
+
+def get_all_values(dictionary):
+    return list(dictionary.value())
+
+myDict = {
+    'name': 'Kobe',
+    'team': 'LA Lakers',
+    'position': 'Shooting Guard'
+}
