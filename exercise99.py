9f7825f3b6d783b2399c8d800939068dca9436ab
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-26 21:26:16 +0545

    99

--- exercise99.py
+++ exercise99.py
@@ -1,10 +1,14 @@
 # Program to get a list of all values in a dictionary
 
 def get_all_values(dictionary):
-    return list(dictionary.value())
+    return list(dictionary.values())
 
 myDict = {
     'name': 'Kobe',
     'team': 'LA Lakers',
     'position': 'Shooting Guard'
 }
+
+values_list = get_all_values(myDict)
+
+print("List of values: ", values_list)
