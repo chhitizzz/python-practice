a089f72e38914328f083749664b0100da1492b93
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-27 21:26:54 +0545

    100

--- exercise100.py
+++ exercise100.py
@@ -1,1 +1,18 @@
 # Program to check if a given value exists in a dictionary
+
+def check_value_existence(dictionary, value):
+    return value in dictionary.values()
+
+myDict = {
+    'name': 'Viper',
+    'ultimate': "Viper's Pit",
+    'role': 'Controller'
+}
+
+value_to_check = 'Viper'
+
+if check_value_existence(myDict, value_to_check):
+    print(f"The value '{value_to_check}' exists in the dictionary.")
+
+else:
+    print(f"The value '{value_to_check}' does not exist in the dictionary.")
