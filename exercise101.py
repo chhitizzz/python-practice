41b78151d14e9353c706dd819d355ed295c34dc5
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-28 21:29:26 +0545

    101

--- exercise101.py
+++ exercise101.py
@@ -1,1 +1,13 @@
 # Program to iterate over the key-value pairs of a dictionary
+
+def iterate_dictionary(dictionary):
+    for key, value in dictionary.items():
+        print(f"Key: {key}, Value: {value}")
+
+my_dict = {
+    'name': 'Alice', 
+    'age': 30, 
+    'city': 'New York'
+    }
+
+iterate_dictionary(my_dict)
