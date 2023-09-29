4b1e9f8336192f36ef0e2af0ed32086f14e7d15f
ChhitizV <chhitizvaidya10@gmail.com>
2023-09-29 21:28:25 +0545

    102

--- exercise102.py
+++ exercise102.py
@@ -1,1 +1,13 @@
 # Program to merge two dictionaries into a new dictionary
+
+def merge_dicts(dict1, dict2):
+    merged_dict = dict1.copy()  
+    merged_dict.update(dict2) 
+    return merged_dict
+
+dict1 = {'a': 1, 'b': 2}
+dict2 = {'b': 3, 'c': 4}
+
+merged_dict = merge_dicts(dict1, dict2)
+
+print("Merged dictionary:", merged_dict)
