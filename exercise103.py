f4062ad46d5b30eacb481c9ca8b947839da55eb6
ChhitizV <chhitizvaidya10@gmail.com>
2023-10-08 21:38:00 +0545

    103

--- exercise103.py
+++ exercise103.py
@@ -1,1 +1,11 @@
 # Program to sort a dictionary by its keys
+
+def sort_dict_by_keys(dictionary):
+    sorted_dict = {k: dictionary[k] for k in sorted(dictionary.keys())}
+    return sorted_dict
+
+my_dict = {
+    'b': 3,
+    'a': 1,
+    'c': 2
+}
