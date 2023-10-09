53b1538dc24f741700a82b0328dc760b316b0816
ChhitizV <chhitizvaidya10@gmail.com>
2023-10-09 22:15:56 +0545

    104

--- exercise104.py
+++ exercise104.py
@@ -1,1 +1,16 @@
 # Program to find the key corresponding to the maximum value in a dictionary
+
+def key_with_max_value(dictionary):
+    if not dictionary:
+        return None
+    
+    max_value = max(dictionary.values())
+    for key, value in dictionary.items():
+        if value == max_value:
+            return key
+
+my_dict = {'a': 10, 'b': 30, 'c': 20}
+
+max_value_key = key_with_max_value(my_dict)
+
+print("Key corresponding to maximum value:", max_value_key)
