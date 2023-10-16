cbb7186554ac457b9e516246bc187b661e893cc3
ChhitizV <chhitizvaidya10@gmail.com>
2023-10-16 21:22:39 +0545

    107

--- exercise107.py
+++ exercise107.py
@@ -1,1 +1,6 @@
 # Create an instance of the Person class and assign the values "Alice" and 30 to name and age respectively
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
