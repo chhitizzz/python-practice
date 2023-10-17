dfb8a7ce9569c6eae24028248e492a0e8061d8f2
ChhitizV <chhitizvaidya10@gmail.com>
2023-10-17 20:27:10 +0545

    108

--- exercise108.py
+++ exercise108.py
@@ -1,1 +1,6 @@
 # Create a class called `Person` with attributes `name` and `age`. Implement a method `display_info()` that prints the name and age of a person
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
