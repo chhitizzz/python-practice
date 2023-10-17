826ef41856afc261edabcc360f1c515897a8365d
ChhitizV <chhitizvaidya10@gmail.com>
2023-10-17 20:28:31 +0545

    108

--- exercise108.py
+++ exercise108.py
@@ -4,3 +4,6 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+    
+    def display_info(self):
+        print(f"Name: {self.name}, Age: {self.age}")
