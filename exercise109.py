a16ff5248028e8e5e24b9adc59251590c3d08999
ChhitizV <chhitizvaidya10@gmail.com>
2023-10-18 22:02:59 +0545

    109

--- exercise109.py
+++ exercise109.py
@@ -1,1 +1,9 @@
 # Create a class called `Rectangle` with attributes `length` and `width`. Implement a method `calculate_area()` that returns the area of the rectangle
+
+class Rectangle: 
+    def __init__(self, length, width):
+        self.length = length 
+        self.width = width 
+    
+    def calculate_area(self):
+        return self.length * self.width
