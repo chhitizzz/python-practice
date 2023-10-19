18c9460edf60e920c17f8ae9de383886df9012da
ChhitizV <chhitizvaidya10@gmail.com>
2023-10-19 22:17:11 +0545

    110

--- exercise110.py
+++ exercise110.py
@@ -1,1 +1,5 @@
 # Create a class called `Circle` with the attribute `radius`. Implement methods `calculate_circumference()` and `calculate_area()` that return the circumference and area of the circle, respectively
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
