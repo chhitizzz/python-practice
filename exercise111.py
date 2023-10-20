f48b611e88a118fbb5eb1aeba1f87b7e73c7b412
ChhitizV <chhitizvaidya10@gmail.com>
2023-10-20 22:08:01 +0545

    111

--- exercise111.py
+++ exercise111.py
@@ -1,1 +1,6 @@
 # Create a class called `Student` with attributes `name` and `grade`. Implement a method `get_grade()` that returns the grade of the student as a letter (A, B, C, etc.) based on a grading scale
+
+class Student:
+    def __init__(self, name, grade):
+        self.name = name
+        self.grade = grade
