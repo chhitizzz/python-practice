e6b809d3aff0c484a8a57fea97be9d494beca6e4
ChhitizV <chhitizvaidya10@gmail.com>
2023-10-20 22:10:46 +0545

    111

--- exercise111.py
+++ exercise111.py
@@ -4,3 +4,22 @@ class Student:
     def __init__(self, name, grade):
         self.name = name
         self.grade = grade
+
+    def get_grade(self):
+        if 90 <= self.grade <= 100:
+            return 'A'
+        
+        elif 80 <= self.grade <= 90:
+            return 'B'
+        
+        elif 70 <= self.grade <= 80:
+            return 'C'
+        
+        elif 60 <= self.grade <= 70:
+            return 'D'
+
+        elif 0 <= self.grade < 60:
+            return 'F'
+        
+        else:
+            return 'Invalid'
