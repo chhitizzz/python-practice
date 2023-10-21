1ca1d7ac1d5ab08830da9a185bf4b4a1b45d8cc1
ChhitizV <chhitizvaidya10@gmail.com>
2023-10-21 22:08:46 +0545

    112

--- exercise112.py
+++ exercise112.py
@@ -1,1 +1,6 @@
 # Create a class called `BankAccount` with attributes `account_number` and `balance`. Implement methods `deposit()` and `withdraw()` that update the account balance accordingly
+
+class BankAccount:
+    def __init__(self, account_number, balance):
+        self.account_number = account_number
+        self.balance = balance
