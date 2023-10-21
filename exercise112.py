25f0b2ee6e00012914ec5fefd4e0c51354a40df4
ChhitizV <chhitizvaidya10@gmail.com>
2023-10-21 22:11:48 +0545

    112

--- exercise112.py
+++ exercise112.py
@@ -1,6 +1,13 @@
 # Create a class called `BankAccount` with attributes `account_number` and `balance`. Implement methods `deposit()` and `withdraw()` that update the account balance accordingly
 
 class BankAccount:
-    def __init__(self, account_number, balance):
+    def __init__(self, account_number, balance=0):
         self.account_number = account_number
         self.balance = balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount 
+            return f"Deposited: Rs. {amount}. New balance: Rs. {self.balance}"
+        else:
+            return "Invalid deposit amount."
