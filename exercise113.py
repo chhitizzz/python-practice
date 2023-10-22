61a920487246383ab4a36c841066ea20a985da8d
ChhitizV <chhitizvaidya10@gmail.com>
2023-10-22 21:35:33 +0545

    113

--- exercise113.py
+++ exercise113.py
@@ -1,1 +1,7 @@
 # Create a class called `Car` with attributes `make`, `model`, and `year`. Implement a method `start_engine()` that prints a message indicating that the car engine has started
+
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year 
