576e99b89f85100cacc73af79c57557d20b99ba6
ChhitizV <chhitizvaidya10@gmail.com>
2023-10-23 18:01:09 +0545

    114

--- exercise114.py
+++ exercise114.py
@@ -1,1 +1,8 @@
 # Create a class called `Book` with attributes `title`, `author`, and `year`. Implement a method `get_book_info()` that prints the title, author, and year of the book
+
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year 
+    
