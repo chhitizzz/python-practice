e6a5228fa884c7e82afcf3dc64848bd8b70b83db
ChhitizV <chhitizvaidya10@gmail.com>
2023-10-25 18:17:33 +0545

    115

--- exercise115.py
+++ exercise115.py
@@ -1,1 +1,7 @@
 # Create a class called `Triangle` with attributes `base` and `height`. Implement a method `calculate_area()` that returns the area of the triangle
+
+class Triangle:
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height 
+    
