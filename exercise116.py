97ca343a50a9a987c50bd8bd2b01cc90e6e76c6a
ChhitizV <chhitizvaidya10@gmail.com>
2023-10-26 21:01:51 +0545

    116

--- exercise116.py
+++ exercise116.py
@@ -1,1 +1,5 @@
 # Create a class called `Dog` with attribute `name`. Implement a method `bark()` that prints a message indicating that the dog is barking
+
+class Dog:
+    def __init__(self, name):
+        self.name = name 
