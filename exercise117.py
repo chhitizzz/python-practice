102252fb6516bdee315f504bef71c6f7bb43a02a
ChhitizV <chhitizvaidya10@gmail.com>
2023-10-27 19:58:24 +0545

    117

--- exercise117.py
+++ exercise117.py
@@ -1,1 +1,7 @@
 # Create a class called `Employee` with attributes `name`, `position`, and `salary`. Implement a method `calculate_bonus()` that returns a bonus amount based on the employee's position
+
+class Employee:
+    def __init__(self, name, position, salary):
+        self.name = name 
+        self.position = position 
+        self.salary = salary 
