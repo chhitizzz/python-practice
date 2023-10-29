4159930494de6b233be3f3057ff8be4fc8adb67c
ChhitizV <chhitizvaidya10@gmail.com>
2023-10-29 16:46:35 +0545

    118

--- exercise118.py
+++ exercise118.py
@@ -1,1 +1,8 @@
 # Create a Student class with attributes like name, age, and grade. Write a program that allows you to create student objects and display their information
+
+class Student:
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age 
+        self.grade = grade 
+        
